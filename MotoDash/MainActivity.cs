5b32f1814f680567266b73bcabeee317d6daeaee
Michael Marelli <Michael Marelli>
2017-09-28 12:33:19 -0700

    Add project files.

---
+++ MotoDash/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace MotoDash
+{
+    [Activity(Label = "MotoDash", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        Button speedometerButton;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            speedometerButton = FindViewById<Button>(Resource.Id.SpeedometerButton);
+
+            speedometerButton.Click += delegate
+            {
+                StartActivity(typeof(Speedometer));
+            };
+        }
+    }
+}
