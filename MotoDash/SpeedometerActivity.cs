619aaed3cb91f189c6707b7080a8582e7caa7ff2
Michael Marelli <MichaelJMarelli@gmail.com>
2017-10-03 17:49:04 -0700

    Moving to desktop

--- MotoDash/SpeedometerActivity.cs
+++ MotoDash/SpeedometerActivity.cs
@@ -9,17 +9,35 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Locations;
+using System.Threading;
 
 namespace MotoDash
 {
     [Activity(Label = "Speedometer")]
     public class SpeedometerActivity : Activity
     {
+        Location location;
+        int refreshRate = 2;
+        float speed;
+        float accuracy;
+        
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
+            TimerCallback timerDelegate = new TimerCallback(RefreshCallback);
+
+            Timer refreshTimer = new Timer(timerDelegate, null, 0, 1000/refreshRate);
+
+        }
+
+        void RefreshCallback(Object o)
+        {
+            speed = location.Speed;
+            accuracy = location.Accuracy;
         }
     }
 }
